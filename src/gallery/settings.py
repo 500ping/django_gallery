748de719080d6cb1741f48b15a8aba0bb0a9a0df
500ping <ndt512x@gmail.com>
2021-01-21 16:50:31 +0700

    add docker

--- src/gallery/settings.py
+++ src/gallery/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '#gi$ugr6d$&lu-n^%0h3@1x4s-lmm6#r_wqs2%o(r0$q1=w*p@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 DATABASES = {
   'default': {
       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-      'NAME': 'site',
-      'USER': 'test',
-      'PASSWORD': 'test',
-      'HOST': 'localhost',
+      'NAME': 'postgres',
+      'USER': 'postgres',
+      'PASSWORD': 'postgres',
+      'HOST': 'db',
       'PORT': '5432',
   }
 }
@@ -132,8 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
